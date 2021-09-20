3e619bd0d966f0159a2642a5998318ffe4d674c8
unknown <darshankumbhar14@gmail.com>
2021-09-20 23:49:25 +0530

    Node Js

--- src/components/UI/BackDrop/BackDrop.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import classes from './BackDrop.module.css'
-const BackDrop = (props) =>
-	props.show ? <div className={classes.Backdrop} onClick={props.clicked_BackDrop}></div> 
-	: null
-export default BackDrop
